c1d24830bdadd7a8a0e85832849d720af7b85235
Paul Gillard <paulmgillard@gmail.com>
2010-01-01 23:34:24 +0000

    Add tests for updating in various ways
    Alter test fixtures to include tracked integer, string and datetime datatypes.
    
    Also add a column which can later be used to test belongs to associations but this isn't intended to be working yet.
    
    These changes were necessary as currently updating fails when multiparameter attributes (e.g. datetime columns) are updated. This is due to the parent column not having the column in its table and ActiveRecord relying on this fact to perform the update.

--- test/dated_detail_test.rb
+++ test/dated_detail_test.rb
@@ -12,27 +12,32 @@ ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":me
 
 def setup_db
   ActiveRecord::Schema.define(:version => 1) do
-    create_table :super_heros do |t|
+    create_table :parrots do |t|
       t.column :name, :string
       t.column :created_at, :datetime
       t.column :updated_at, :datetime
     end
 
-    create_table :super_hero_dated_details do |t|
-      # Ensure any attributes added here are added to relevant tests (search for 'strength')
-      t.column :super_hero_id, :integer
+    create_table :pirates do |t|
+      t.column :name, :string
+      t.column :created_at, :datetime
+      t.column :updated_at, :datetime
+    end
+
+    create_table :pirate_dated_details do |t|
+      # Ensure any attributes added here are added to relevant tests (search for 'catchphrase')
+      t.column :pirate_id, :integer
       t.column :start_on, :datetime
       t.column :end_on, :datetime
-      t.column :strength, :integer
+      t.column :catchphrase, :string
+      t.column :ruthlessness, :integer
+      t.column :birth_date, :datetime
+      t.column :parrot_id, :integer
       t.column :created_at, :datetime
       t.column :updated_at, :datetime
     end
   end
 end
-
-# def create_superhero_with_dated_detail
-#   @superhero = SuperHero.create(:strength => 1)
-# end
 
 def teardown_db
   ActiveRecord::Base.connection.tables.each do |table|
@@ -42,11 +47,21 @@ end
 
 setup_db
 
-class SuperHeroDatedDetail < ActiveRecord::Base
+class Parrot < ActiveRecord::Base
+  has_one :pirate
+  
+  def ==(other)
+    name == other.name
+  end
+end
+
+class PirateDatedDetail < ActiveRecord::Base
   acts_as_dated_detail
 end
 
-class SuperHero < ActiveRecord::Base
+class Pirate < ActiveRecord::Base
+  belongs_to :parrot
+  
   acts_as_dated
 end
 
@@ -66,20 +81,32 @@ class ActsAsDatedDetailTest < Test::Unit::TestCase
   
   private
   
-  def create_superhero(times = [Time.now])
+  def create_parrot(time)
+    parrot = Parrot.create!(:name => time.to_s)
+  end
+  
+  def create_pirate(times = [Time.now])
     first_time = times.shift
     now = Time.now
     Time.stubs(:now).returns(first_time)
-    superhero = SuperHero.create!(:strength => strength(first_time))
+    pirate = Pirate.create!(:name => pirate_name(first_time), :catchphrase => catchphrase(first_time), :ruthlessness => ruthlessness(first_time), :birth_date => first_time, :parrot => create_parrot(first_time))
     times.each do |time|
       Time.stubs(:now).returns(time)
-      superhero.update_attribute(:strength, strength(time))
+      pirate.update_attributes(:name => pirate_name(time), :catchphrase => catchphrase(time), :ruthlessness => ruthlessness(time), :birth_date => time, :parrot => create_parrot(time))
     end
     Time.stubs(:now).returns(now)
-    superhero
+    pirate
   end
   
-  def strength(time)
+  def pirate_name(time)
+    time.to_s
+  end
+  
+  def catchphrase(time)
+    time.to_s
+  end
+  
+  def ruthlessness(time)
     time.to_i
   end
 end
@@ -88,8 +115,8 @@ class ParentTest < ActsAsDatedDetailTest
   # Creation
   
   def test_related_dated_detail_created_along_with_model
-    superhero = SuperHero.create!
-    assert_equal 1, superhero.dated_details.count
+    pirate = Pirate.create!
+    assert_equal 1, pirate.dated_details.count
   end
   
   # Currently Effective Timestamp
@@ -97,37 +124,37 @@ class ParentTest < ActsAsDatedDetailTest
   def test_default_value_of_currently_effective_timestamp
     now = Time.now
     Time.stubs(:now).returns(now)
-    superhero = SuperHero.create!
-    assert_equal Time.now, superhero.on
+    pirate = Pirate.create!
+    assert_equal Time.now, pirate.on
   end
   
   def test_setting_value_of_currently_effective_timestamp
     now = Time.now
     Time.stubs(:now).returns(now)
-    superhero = SuperHero.create!
+    pirate = Pirate.create!
     one_month_ago = Time.now - 1.month
-    superhero.on = one_month_ago
-    assert_equal one_month_ago, superhero.on
+    pirate.on = one_month_ago
+    assert_equal one_month_ago, pirate.on
   end
   
   # Tracked Attribute Retrieval
   
   def test_tracked_attribute_for_oldest_timestamp_set_by_instance_method
-    superhero = create_superhero([1.year.ago, 6.months.ago, 1.month.ago])
-    superhero.on = 8.months.ago
-    assert_equal strength(1.year.ago), superhero.strength
+    pirate = create_pirate([1.year.ago, 6.months.ago, 1.month.ago])
+    pirate.on = 8.months.ago
+    assert_equal catchphrase(1.year.ago), pirate.catchphrase
   end
   
   def test_tracked_attribute_for_middle_timestamp_set_by_instance_method
-    superhero = create_superhero([1.year.ago, 6.months.ago, 1.month.ago])
-    superhero.on = 5.months.ago
-    assert_equal strength(6.months.ago), superhero.strength
+    pirate = create_pirate([1.year.ago, 6.months.ago, 1.month.ago])
+    pirate.on = 5.months.ago
+    assert_equal catchphrase(6.months.ago), pirate.catchphrase
   end
   
   def test_tracked_attribute_for_newest_timestamp_set_by_instance_method
-    superhero = create_superhero([1.year.ago, 6.months.ago, 1.month.ago])
-    superhero.on = 2.weeks.ago
-    assert_equal strength(1.month.ago), superhero.strength
+    pirate = create_pirate([1.year.ago, 6.months.ago, 1.month.ago])
+    pirate.on = 2.weeks.ago
+    assert_equal catchphrase(1.month.ago), pirate.catchphrase
   end
 
   # def test_start_on
@@ -162,51 +189,79 @@ class ParentTest < ActsAsDatedDetailTest
   
   def test_read_tracked_attributes
     # Ensure value from dated_detail is returned
-    superhero = SuperHero.create!
-    dated_detail = superhero.dated_detail
-    superhero.stubs(:dated_detail).returns(dated_detail)
-    SuperHeroDatedDetail.tracked_attributes.each do |attribute|
+    pirate = Pirate.create!
+    dated_detail = pirate.dated_detail
+    pirate.stubs(:dated_detail).returns(dated_detail)
+    PirateDatedDetail.tracked_attributes.each do |attribute|
       value = 10
-      SuperHeroDatedDetail.any_instance.stubs(attribute).returns("Error")
+      PirateDatedDetail.any_instance.stubs(attribute).returns("Error")
       dated_detail.stubs(attribute).returns(value)
-      assert_equal value, superhero.send(attribute)
+      assert_equal value, pirate.send(attribute)
     end
   end
   
   def test_write_tracked_attributes
-    superhero = SuperHero.create!
-    SuperHeroDatedDetail.tracked_attributes.each do |attribute|
+    pirate = Pirate.create!
+    PirateDatedDetail.tracked_attributes.each do |attribute|
       value = 10
-      superhero.send("#{attribute}=", value)
-      assert_equal value, superhero.dated_detail.send(attribute)
+      pirate.send("#{attribute}=", value)
+      assert_equal value, pirate.dated_detail.send(attribute)
     end
   end
   
   # Updating Attributes
   
-  def test_updating_tracked_attribute
+  def test_updating_tracked_integer_attribute
     now = Time.now
     Time.stubs(:now).returns(now - 1.year)
-    superhero = SuperHero.create!
+    pirate = Pirate.create!
     Time.stubs(:now).returns(now)
-    superhero.update_attribute(:strength, 10)
-    assert_equal 2, superhero.dated_details.count
+    pirate.update_attribute(:ruthlessness, 10)
+    assert_equal 2, pirate.dated_details.count
+  end
+  
+  def test_updating_tracked_string_attribute
+    now = Time.now
+    Time.stubs(:now).returns(now - 1.year)
+    pirate = Pirate.create!
+    Time.stubs(:now).returns(now)
+    pirate.update_attribute(:catchphrase, 'Yar!')
+    assert_equal 2, pirate.dated_details.count
+  end
+  
+  def test_updating_tracked_datetime_attribute
+    now = Time.now
+    Time.stubs(:now).returns(now - 1.year)
+    pirate = create_pirate
+    Time.stubs(:now).returns(now)
+    pirate.update_attributes(:birth_date => pirate.birth_date + 1.day)
+    assert_equal 2, pirate.dated_details.count
+  end
+  
+  def test_updating_tracked_multiparameter_attribute
+    now = Time.now
+    Time.stubs(:now).returns(now - 1.year)
+    pirate = create_pirate
+    Time.stubs(:now).returns(now)
+    new_birth_date = pirate.birth_date + 1.day
+    pirate.update_attributes('birth_date(1i)' => "#{new_birth_date.year}", 'birth_date(2i)' => "#{new_birth_date.month}", 'birth_date(3i)' => "#{new_birth_date.day}")
+    assert_equal 2, pirate.dated_details.count
   end
   
   def test_updating_untracked_attribute
     now = Time.now
     Time.stubs(:now).returns(now - 1.year)
-    superhero = SuperHero.create!
+    pirate = Pirate.create!
     Time.stubs(:now).returns(now)
-    superhero.update_attribute(:name, 'Batman')
-    assert_equal 1, superhero.dated_details.count
+    pirate.update_attribute(:name, 'Long John Silver')
+    assert_equal 1, pirate.dated_details.count
   end
 end
 
 class DatedDetailTest < ActsAsDatedDetailTest
   def setup
     setup_db
-    # create_superhero_with_dated_detail
+    # create_pirate_with_dated_detail
   end
 
   def teardown
@@ -216,19 +271,19 @@ class DatedDetailTest < ActsAsDatedDetailTest
   # Associations
   
   def test_belongs_to_parent
-    assert_equal create_superhero, SuperHeroDatedDetail.first.super_hero
+    assert_equal create_pirate, PirateDatedDetail.first.pirate
   end
   
   # Creation
   
   def test_initial_start_on_value
-    superhero = SuperHero.create!
-    assert_equal Time.now.to_i, SuperHeroDatedDetail.first.start_on.to_i
+    pirate = Pirate.create!
+    assert_equal Time.now.to_i, PirateDatedDetail.first.start_on.to_i
   end
   
   def test_initial_end_on_value
-    superhero = SuperHero.create!
-    assert_nil SuperHeroDatedDetail.first.end_on
+    pirate = Pirate.create!
+    assert_nil PirateDatedDetail.first.end_on
   end
   
   # Updating
@@ -236,18 +291,18 @@ class DatedDetailTest < ActsAsDatedDetailTest
   def test_updating
     now = Time.now
     Time.stubs(:now).returns(now - 1.year)
-    superhero = SuperHero.create!
+    pirate = Pirate.create!
     Time.stubs(:now).returns(now)
     
-    dated_detail = superhero.dated_detail
+    dated_detail = pirate.dated_detail
     original_dated_detail = dated_detail.class.find(dated_detail.id) # Cloning would keep millisecond parts of time which would make later comparisons harder
     
-    dated_detail.update_attribute(:strength, 10)
+    dated_detail.update_attribute(:ruthlessness, 10)
     
     assert_equal now.to_i, dated_detail.start_on.to_i
     assert_nil dated_detail.end_on
     
-    previous_dated_detail = SuperHeroDatedDetail.find_by_start_on(original_dated_detail.start_on)
+    previous_dated_detail = PirateDatedDetail.find_by_start_on(original_dated_detail.start_on)
     assert_equal dated_detail.start_on.to_i - 1, previous_dated_detail.end_on.to_i
     assert_equal original_dated_detail.attributes.except('end_on', 'id'), previous_dated_detail.attributes.except('end_on', 'id')
   end
@@ -255,47 +310,50 @@ class DatedDetailTest < ActsAsDatedDetailTest
   # Tracked Attributes
   
   def test_tracked_attributes_includes_relevant_columns
-    assert SuperHeroDatedDetail.tracked_attributes.include?('strength')
+    assert PirateDatedDetail.tracked_attributes.include?('catchphrase')
+    assert PirateDatedDetail.tracked_attributes.include?('ruthlessness')
+    assert PirateDatedDetail.tracked_attributes.include?('birth_date')
+    assert PirateDatedDetail.tracked_attributes.include?('parrot_id')
   end
   
   def test_tracked_attributes_excludes_id
-    assert !SuperHeroDatedDetail.tracked_attributes.include?('id')
+    assert !PirateDatedDetail.tracked_attributes.include?('id')
   end
   
   def test_tracked_attributes_excludes_start_on
-    assert !SuperHeroDatedDetail.tracked_attributes.include?('start_on')
+    assert !PirateDatedDetail.tracked_attributes.include?('start_on')
   end
   
   def test_tracked_attributes_excludes_end_on
-    assert !SuperHeroDatedDetail.tracked_attributes.include?('end_on')
+    assert !PirateDatedDetail.tracked_attributes.include?('end_on')
   end
   
   def test_tracked_attributes_excludes_updated_at
-    assert !SuperHeroDatedDetail.tracked_attributes.include?('updated_at')
+    assert !PirateDatedDetail.tracked_attributes.include?('updated_at')
   end
   
   def test_tracked_attributes_excludes_created_at
-    assert !SuperHeroDatedDetail.tracked_attributes.include?('created_at')
+    assert !PirateDatedDetail.tracked_attributes.include?('created_at')
   end
   
   def test_tracked_attributes_excludes_parent_id
-    assert !SuperHeroDatedDetail.tracked_attributes.include?('super_hero_id')
+    assert !PirateDatedDetail.tracked_attributes.include?('pirate_id')
   end
   
   # Dated Detail Retrieval
   
   def test_dated_detail_for_oldest_timestamp_retrieved_via_named_scope
-    superhero = create_superhero([1.year.ago, 6.months.ago, 1.month.ago])
-    assert_equal strength(1.year.ago), superhero.dated_details.on(8.months.ago).first.strength
+    pirate = create_pirate([1.year.ago, 6.months.ago, 1.month.ago])
+    assert_equal catchphrase(1.year.ago), pirate.dated_details.on(8.months.ago).first.catchphrase
   end
   
   def test_tracked_attribute_for_middle_timestamp_retrieved_via_named_scope
-    superhero = create_superhero([1.year.ago, 6.months.ago, 1.month.ago])
-    assert_equal strength(6.months.ago), superhero.dated_details.on(5.months.ago).first.strength
+    pirate = create_pirate([1.year.ago, 6.months.ago, 1.month.ago])
+    assert_equal catchphrase(6.months.ago), pirate.dated_details.on(5.months.ago).first.catchphrase
   end
   
   def test_tracked_attribute_for_newest_timestamp_retrieved_via_named_scope
-    superhero = create_superhero([1.year.ago, 6.months.ago, 1.month.ago])
-    assert_equal strength(1.month.ago), superhero.dated_details.on(2.weeks.ago).first.strength
+    pirate = create_pirate([1.year.ago, 6.months.ago, 1.month.ago])
+    assert_equal catchphrase(1.month.ago), pirate.dated_details.on(2.weeks.ago).first.catchphrase
   end
 end
